7665822f6b73521b3fa1fda47336c0c8ce34711d
manishprajapati369108-blip <manishprajapati369108@gmail.com>
2026-07-11 09:57:21 +0530

    backend completed

--- server/router/fileStorage.js
+++ server/router/fileStorage.js
@@ -5,5 +5,9 @@ import {upload, view , download, removeOne, removeAll} from "../controllers/file
 const router = express.Router();
 
 router.post("/upload", upload.single("file"), upload);
+router.get("/view", view );
+router.get("/download", download);
+router.delete("/remove-one", removeOne);
+router.delete("/remove-all", removeAll );
 
 export default router; 
