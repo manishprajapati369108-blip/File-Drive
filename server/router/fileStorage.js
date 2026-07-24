09c7e87d37d44864b439481c92a8fcd02a9fdd1b
manishprajapati369108-blip <manishprajapati369108@gmail.com>
2026-07-24 10:47:16 +0530

    practice git

--- server/router/fileStorage.js
+++ server/router/fileStorage.js
@@ -14,7 +14,7 @@ const router = express.Router();
 
 router.post("/upload",protect, uploadMiddleware.array("files"), upload);
 router.get("/list",protect, list)
-router.get("/view/:filePath",protect, view);
+router.get("/view/:filePathed",protect, view);
 router.get("/download/:filePath", protect,download);
 router.delete("/remove-one/:filePath",protect, removeOne);
 router.delete("/remove-all",protect, removeAll);
