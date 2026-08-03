fbd4411bfb45259e6ca8e539bf0d7285cd3596dd
manishprajapati369108-blip <manishprajapati369108@gmail.com>
2026-08-03 13:04:43 +0530

    a

--- server/server.js
+++ server/server.js
@@ -2,35 +2,39 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./router/authRoutes.js";
-import forgetPassword from "./router/forgetPassword.js"
+import forgetPassword from "./router/forgetPassword.js";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
 import fileStorage from "./router/fileStorage.js";
 
-dotenv.config({path: "../.env" })
-
+dotenv.config({ path: "../.env" });
 
 const app = express();
-app.use(cors({ credentials: true}));
+app.use(
+  cors({
+    origin: "https://file-drive-ten.vercel.app",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const port = process.env.PORT || 5000;
- 
+
 app.use("/auth", authRoutes);
 app.use("/otp", forgetPassword);
 app.use("/file", fileStorage);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    message: 'File Drive API is running!',
-    status: '✅ Online',
+    message: "File Drive API is running!",
+    status: "✅ Online",
     endpoints: {
-      auth: '/auth',
-      files: '/file',
-      otp: '/otp'
-    }
+      auth: "/auth",
+      files: "/file",
+      otp: "/otp",
+    },
   });
 });
 
