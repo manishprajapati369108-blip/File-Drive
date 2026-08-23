99c5c231628254901cda84ae127cdf0e2a04d59c
manishprajapati369108-blip <manishprajapati369108@gmail.com>
2026-08-23 22:45:32 +0530

    bcrypt solve

--- server/router/authRoutes.js
+++ server/router/authRoutes.js
@@ -30,12 +30,9 @@ router.post("/register", async (req, res) => {
       });
     }
 
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-
     const user = new User({
       email: email,
-      password: hashedPassword,
+      password: password,
     });
 
     await user.save();
